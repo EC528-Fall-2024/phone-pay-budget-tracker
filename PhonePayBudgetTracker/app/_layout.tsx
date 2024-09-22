ca3e79666084f7e1ee1298c184c386ede5af7de8
brennanmahoney <bmm1@bu.edu>
2024-09-22 19:08:37 -0400

    Added index page

--- PhonePayBudgetTracker/app/_layout.tsx
+++ PhonePayBudgetTracker/app/_layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
