35152db70aa11357418739f1f9041ace31ffcc8f
Naomi Gonzalez <ngonzalz@bu.edu>
2024-11-17 03:23:48 -0500

    deployed dynamodbs and apigateway

--- mainInfrastructure/deploy.sh
+++ mainInfrastructure/deploy.sh
@@ -3,9 +3,9 @@
 set -e
 
 # Set the environment variables
-S3_BUCKET_NAME="NEED_TO_BE_REPLACED"
-REGION="NEED_TO_BE_REPLACED"
-INFRA_STACK_NAME="NEED_TO_BE_REPLACED"
+S3_BUCKET_NAME="phonepay"
+REGION="us-east-2"
+INFRA_STACK_NAME="main-infra2"
 
 # Deploy main infrastructure
 echo "Deploying main infrastructure..."
@@ -23,28 +23,28 @@ echo "PROFILE_DATA_TABLE_NAME: $PROFILE_DATA_TABLE_NAME"
 TRANSACTION_TABLE=$(aws cloudformation describe-stacks --stack-name $INFRA_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='TransactionTableName'].OutputValue" --output text)
 echo "TRANSACTION_TABLE: $TRANSACTION_TABLE"
 
-# Deploy user profile service
-echo "Deploying user profile service..."
-cd microservices/User-Profile/
-sam build
-sam deploy \
-  --stack-name user-profile-service \
-  --s3-bucket $S3_BUCKET_NAME \
-  --capabilities CAPABILITY_IAM \
-  --parameter-overrides \
-    ProfileDataTableName=$PROFILE_DATA_TABLE_NAME \
-    SharedApiGatewayId=$API_GATEWAY_ID
-cd ../..
+# # Deploy user profile service
+# echo "Deploying user profile service..."
+# cd microservices/User-Profile/
+# sam build
+# sam deploy \
+#   --stack-name user-profile-service \
+#   --s3-bucket $S3_BUCKET_NAME \
+#   --capabilities CAPABILITY_IAM \
+#   --parameter-overrides \
+#     ProfileDataTableName=$PROFILE_DATA_TABLE_NAME \
+#     SharedApiGatewayId=$API_GATEWAY_ID
+# cd ../..
 
-# Deploy admin control service
-echo "Deploying admin control service..."
-cd microservices/admin-control/
-sam build
-sam deploy \
-  --stack-name admin-control-service \
-  --s3-bucket $S3_BUCKET_NAME \
-  --capabilities CAPABILITY_IAM \
-  --parameter-overrides \
-    ProfileDataTableName=$PROFILE_DATA_TABLE_NAME \
-    ApiGatewayId=$API_GATEWAY_ID
-cd ../..
+# # Deploy admin control service
+# echo "Deploying admin control service..."
+# cd microservices/admin-control/
+# sam build
+# sam deploy \
+#   --stack-name admin-control-service \
+#   --s3-bucket $S3_BUCKET_NAME \
+#   --capabilities CAPABILITY_IAM \
+#   --parameter-overrides \
+#     ProfileDataTableName=$PROFILE_DATA_TABLE_NAME \
+#     ApiGatewayId=$API_GATEWAY_ID
+# cd ../..
